10e1214cd5b2b4f10212ff23e74dcf6208b85b69
Sukura <1831294461@qq.com>
2026-01-02 15:09:56 +0800

    addSEO

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ function build() {
   const spinner = ora('Loading unicorns').start();
   const commont = process.argv[2]
   execSync('npm run build')
+  console.log('build 完成')
   gitCommander('add .')
+  console.log('add 完成')
   gitCommander(`commit -m "${commont}"`)
+  console.log('commit 完成')
   gitCommander('push origin main')
-  spinner.succeed('打包完成')
+  spinner.succeed('执行完成')
 }
 
 
