c8ae5d0cd6eb640851946843b67ed65ce83b8ef4
Sakura <1831294461@qq.com>
2026-03-06 04:02:21 +0800

    添加域名

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-import { execSync } from 'child_process' //子进程 用于执行shell命令
-import ora from 'ora'
-import fs from 'node:fs'
+import { execSync } from "child_process"; //子进程 用于执行shell命令
+import ora from "ora";
+import fs from "node:fs";
 
 const clearFolder = () => {
-  if (fs.existsSync('./docs')) {
-    fs.rmdirSync('./docs', { recursive: true })
+  if (fs.existsSync("./docs")) {
+    fs.rmdirSync("./docs", { recursive: true });
   }
-}
-
+};
 
 const gitCommander = (commad) => {
-  execSync(`git ${commad}`)
-}
+  execSync(`git ${commad}`);
+};
 function build() {
-  clearFolder()
-  const spinner = ora('开始执行').start();
-  const commont = process.argv[2]
-  execSync('npm run build')
-  console.log('build 完成')
-  gitCommander('add .')
-  console.log('add 完成')
-  gitCommander(`commit -m "${commont}"`)
-  console.log('commit 完成')
-  gitCommander('push origin main')
-  spinner.succeed('执行完成')
+  clearFolder();
+  const spinner = ora("开始执行").start();
+  const commont = process.argv[2];
+  execSync("npm run build");
+  console.log("build 完成");
+  gitCommander("add .");
+  console.log("add 完成");
+  gitCommander(`commit -m "${commont}"`);
+  console.log("commit 完成");
+  gitCommander("push origin main");
+  spinner.succeed("执行完成");
 }
-
 
 function main() {
-  build()
+  build();
 }
 
-main()
+main();
